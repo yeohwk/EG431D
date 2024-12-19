3289f5985ebf9dc0cad9e343ac8ed1111e43b559
yeohwk <yeohwk@gmail.com>
2024-12-19 08:43:47 +0800

    Update lesson materials and source codes

--- Lesson6/coreBodyTempMon/coreBodyTempMon.ino
+++ Lesson6/coreBodyTempMon/coreBodyTempMon.ino
@@ -1,42 +1,65 @@
 #include <math.h>
 #include <Wire.h>
 #include "rgb_lcd.h"
-#include "Seeed_AMG8833_driver.h"
+#include "Adafruit_AMG88xx.h"   // Replace with the new library from Adafruit
+#include <MovingAverageFilter.h>
 
-#define TEMP_THRESHOLD    40    // Max Temperature Threshold (degree C)
-#define VIBR_THRESHOLD    1.9   // Vibration Threshold (g)
+#define BUZZER_MODULE     2
+#define NUM_POINTS        32    // Filter Size (number of measurement points)
 
-rgb_lcd lcd;                    // Initialize LCD instance
-AMG8833 tsa(0x69);              // Initialize AMG8833 instance of the  AMG8833 TSA library (use address 0x68 or 0x69)
+#define STATE_NORMAL      1     // State ID for STATE NORMAL
+#define STATE_FEVER       2     // State ID for STATE FEVER
+#define STATE_MFEVER      3     // State ID for STATE MODERATE FEVER   
+#define STATE_HFEVER      4     // State ID for STATE HIGH FEVER       
+
+rgb_lcd lcd;                                          // Initialize LCD instance
+Adafruit_AMG88xx tsa;                                 // Create an AMG8833 instance
+MovingAverageFilter movingAverageFilter(NUM_POINTS);  // Initialize Moving Average Filter
 
 float getMaximumTargetTemperature(unsigned int* index)
 {
-    float pixelsTemperature[PIXEL_NUM] = {0};
-    float maxTemp = 0;
-    tsa.read_pixel_temperature(pixelsTemperature);     // Read temperature of all the 64 pixels
+    float pixelsTemperature[AMG88xx_PIXEL_ARRAY_SIZE] = {0};
+    float apparentTemp = 0;
+    double targetTemp  = 0;
+    double ambientTemp = 0;
 
+    // Read temperature of all the 64 pixels (temperature sensors)
+    tsa.readPixels(pixelsTemperature);                
+    // Get the maximum apparent temperature value from the 64 pixels
     for(int i = 0; i < 64; i++)
     {
-        if(pixelsTemperature[i] > maxTemp)
+        if(pixelsTemperature[i] > apparentTemp)
         {
-            maxTemp = pixelsTemperature[i];
+            apparentTemp = pixelsTemperature[i];
             *index = i;
         }
     }
     // At this point, maxTemp is the apparent temperature measured by the TSA.
-    // Need to apply the provided equation to obtain the Target Temperature. 
-
-    
-
-    return maxTemp;    
+    // Need to pre-process with a moving average filter to remove noise.
+    // Need to apply the provided equation to obtain the Target Temperature.
+    apparentTemp = movingAverageFilter.process(apparentTemp);         // Pre-process with moving average filter
+    apparentTemp = apparentTemp + (float) 273.15;                     // Converts apparent temperature from deg C to Kelvin 
+    ambientTemp  = (double) tsa.readThermistor() + (double) 273.15;   // Read Thermistor temperature (ambient temperature) and converts to Kelvin
+    ambientTemp  = pow(ambientTemp, (double) 4.0);                    // Implementation of formula to calculate target temperature from measured 
+    ambientTemp  = (double) (0.02) * ambientTemp;                     // apparent temperature, ambient temperature and emissivity values.
+    targetTemp   = pow((double) apparentTemp, (double) 4.0);          // 
+    targetTemp   = targetTemp - ambientTemp;                          //
+    targetTemp   = targetTemp / (double) 0.98;                        //  
+    targetTemp   = sqrt(targetTemp);                                  //
+    targetTemp   = sqrt(targetTemp);                                  //
+    return (float) (targetTemp) - (float) 273.15;                     // Calculated target temperature converts to deg C
 }
 
 void setup() 
 {
-    Serial.begin(2000000);                                      // Initialize Serial Port 
+    bool status;
+    Wire.begin();
+    Serial.begin(2000000);                                      // Initialize Serial Port
+    pinMode(BUZZER_MODULE, OUTPUT);                             // Set BUZZER_MODULE (D2) as digital output 
     lcd.begin(16, 2);                                           // Initialize LCD (LCD Start!)
     lcd.setRGB(0, 255, 0);                                      // Green LCD Back Light
-    if (tsa.init()) {                                           // Initialize AMG8833 TSA
+    status = tsa.begin(0x69, &Wire);                            // Initialize TSA (use address 0x68 or 0x69)
+    if (status == 0) {                                          // Check TSA initialization status.
         Serial.println("TSA Init Failed!!!");                   // If TSA initialize failed, Print Failed Message to Serial Port
         lcd.setRGB(255, 0, 0);                                  // Set Red LCD Back Light
         lcd.print("TSA FAILED!");                               // Print Failed Message to LCD
@@ -47,36 +70,125 @@ void setup()
 
 void loop() 
 {
-    static unsigned int maxTempIndex = 0;                                  // Index of Maximum Temperature on TSA 
-    static float maxTemp = 0.0;                                            // Maximum Temperature
-    maxTemp = getMaximumTargetTemperature(&maxTempIndex);                  // Read Maximum non-contact Temperature and its Index from TSA 
+    // STATE variables
+    static unsigned char STATE = STATE_NORMAL;                  // Store State ID
+    static unsigned int TB_I = 0;                               // CBT Index on TSA 
+    static float TB = 0.0;                                      // CBT (Maximum non-contact temperature)
+    static unsigned int BUZ_CT = 0;
+    static bool BUZZER = false;
 
+    // Read sensors (Input)
+    // Read maximum non-contact temperature (CBT) and its Index from TSA 
+    TB = getMaximumTargetTemperature(&TB_I);
 
-
-
-
-    // Implements simple thresholds checks for monitoring of machine health (Todo: Need Improvements!)
-    if(soundLevel > SOUND_THRESHOLD || maxTemp > TEMP_THRESHOLD || xyzMag > VIBR_THRESHOLD)
+    // Run Finite State Machine (FSM) logic
+    switch (STATE)
     {
-        lcd.clear();
-        lcd.setRGB(255, 0, 0);                                           // Set Red LCD Back Light
-        lcd.print("[W] Mac Check!");                                     // Print Warning Message to LCD when threshold exceeded
+        case STATE_NORMAL:
+          if (TB < 37.5)
+          {}
+          else if (TB>=37.5 && TB < 38.3)
+          {
+              STATE = STATE_FEVER;
+              BUZ_CT = 4;
+          }
+          else if (TB>=38.3 && TB < 39)
+          {
+              STATE = STATE_MFEVER;
+          }
+          else if (TB >= 39)
+          {
+              STATE = STATE_HFEVER;
+              BUZZER = true;
+          }
+          else
+          {}
+        break;
+        case STATE_FEVER:
+          if (TB>=37.5 && TB < 38.3 && BUZ_CT == 0)
+          {
+              BUZ_CT = 4;
+              BUZZER = true;
+          }
+          else if (TB>=37.5 && TB < 38.3 && BUZ_CT > 0)
+          {
+              BUZ_CT = BUZ_CT - 1;
+              BUZZER = false;              
+          }
+          else if (TB < 37.5 || TB >= 38.3)
+          {
+              STATE = STATE_NORMAL;
+              BUZZER = false;
+          }
+          else
+          {}
+        break;
+        case STATE_MFEVER:
+          if (TB >= 38.3 && TB < 39)
+          {
+              BUZZER = !BUZZER;
+          }
+          else if (TB < 38.3 || TB >= 39)
+          {
+              STATE = STATE_NORMAL;
+              BUZZER = false;
+          }
+          else
+          {}
+        break;
+        case STATE_HFEVER:
+          if (TB >= 39)
+          {}
+          else if (TB < 39)
+          {
+              STATE = STATE_NORMAL;
+              BUZZER = false;
+          }
+          else
+          {}
+        break;
+        default:
+        break;  
+    } 
+
+    // Update Outputs
+    // Print results to LCD and Serial Monitor
+    // Todo: Send results to cloud dashboard for remote monitoring purpose
+    lcd.clear();
+    if (STATE == STATE_NORMAL)
+    {
+      lcd.setRGB(0, 255, 0);                            // Green LCD Back Light 
+      lcd.setCursor(0, 0); lcd.print("[STAT]: "); lcd.print("NORMAL");
+      Serial.print("STATE_NORMAL: ");    
+    }
+    else if (STATE == STATE_FEVER)
+    {
+      lcd.setRGB(0, 0, 255);                            // Blue LCD Back Light 
+      lcd.setCursor(0, 0); lcd.print("[STAT]: "); lcd.print("FEVER!");
+      Serial.print("STATE_FEVER: ");     
+    }
+    else if (STATE == STATE_MFEVER)
+    {
+      lcd.setRGB(255, 255, 0);                          // Yellow LCD Back Light 
+      lcd.setCursor(0, 0); lcd.print("[STAT]: "); lcd.print("MFEVER!");
+      Serial.print("STATE_MFEVER: ");     
     }
     else
     {
-        lcd.clear();
-        lcd.setRGB(0, 255, 0);                                           // Set Green LCD Back Light
-        lcd.print("[I] Mac OK!");                                        // Print Information Message to LCD when thresholds are within range  
-    }    
+      lcd.setRGB(255, 0, 0);                            // Red LCD Back Light 
+      lcd.setCursor(0, 0); lcd.print("[STAT]: "); lcd.print("HFEVER!");
+      Serial.print("STATE_HFEVER: ");       
+    }
+    lcd.setCursor(0, 1); lcd.print("[CBT ]: "); lcd.print(TB);
 
-    // Todo: Send Sensor values to Cloud Dashboard.
+    // Print results to Serial Monitor
+    Serial.print("CBT: ");
+    Serial.print(TB);
+    Serial.print(" deg C, CBTi: ");
+    Serial.println(TB_I);
 
-    // Print all sensor values to Serial Port. 
-    Serial.print(soundLevel);
-    Serial.print(" ");
-    Serial.print(maxTemp);
-    Serial.print(" ");
-//  Serial.print(maxTempIndex);
-//  Serial.print(" ");
-    Serial.println(xyzMag);            
+    // Update BUZZER_MODULE (output module) at every cycle
+    digitalWrite(BUZZER_MODULE, BUZZER);
+    // 100ms FSM time reference (Runs at every 100ms)
+    delay(100);
 }
--- Lesson6/safeViewEnforcer/safeViewEnforcer.ino
+++ Lesson6/safeViewEnforcer/safeViewEnforcer.ino
@@ -25,138 +25,138 @@ void setup()
 
 void loop()
 {
-  // STATE variables
-  static unsigned char STATE   = STATE_SAFE;          // Store State ID
-  static unsigned int  TIMER   = 0;                   // Power-Off Timer (e.g. TIMER = 100 indicates TV to be power off after 100ms x 100 = 10 seconds)
-  static unsigned int  BUZ_CTR = 0;                   // Variable for generating beeping sound (e.g. BUZ_CTR = 5 indicates buzzer to beep at every 500ms)
-  static float DISTANCE = 0;                          // Store distance information from sensor
-  static bool TVON = true;                            // Output variable to indicate TV ON/OFF
-  static bool BUZZER = false;                         // Output variable to indicate Buzzer ON/OFF
+    // STATE variables
+    static unsigned char STATE   = STATE_SAFE;        // Store State ID
+    static unsigned int  TIMER   = 0;                 // Power-Off Timer (e.g. TIMER = 100 indicates TV to be power off after 100ms x 100 = 10 seconds)
+    static unsigned int  BUZ_CTR = 0;                 // Variable for generating beeping sound (e.g. BUZ_CTR = 5 indicates buzzer to beep at every 500ms)
+    static float DISTANCE = 0;                        // Store distance information from sensor
+    static bool TVON = true;                          // Output variable to indicate TV ON/OFF
+    static bool BUZZER = false;                       // Output variable to indicate Buzzer ON/OFF
 
-  // Read sensors (Input)
-  // Read distance value from sensor and feed into a median filter
-  DISTANCE = medianFilter.AddValue(ultrasonic.MeasureInCentimeters());
+    // Read sensors (Input)
+    // Read distance value from sensor and feed into a median filter
+    DISTANCE = medianFilter.AddValue(ultrasonic.MeasureInCentimeters());
 
-  // Run Finite State Machine (FSM) logic
-  switch (STATE)
-  {
-      case STATE_SAFE:
-        if (DISTANCE >= 220)
-        {}
-        else if (DISTANCE >= 150 && DISTANCE < 220)
-        {
-            STATE = STATE_REMINDER;
-            BUZ_CTR = 5;
-        }
-        else if (DISTANCE < 150)
-        {
-            STATE = STATE_WARNING;
-            TIMER = 100;
-        }
-        else
-        {}
-      break;
-      case STATE_REMINDER:
-        if (DISTANCE >= 150 && DISTANCE < 220 && BUZ_CTR > 0)
-        {
-            BUZ_CTR = BUZ_CTR - 1;
-            BUZZER = false;
-        }
-        else if (DISTANCE >= 150 && DISTANCE < 220 && BUZ_CTR == 0)
-        {
-            BUZ_CTR = 5;
-            BUZZER = true;
-        }
-        else if (DISTANCE >= 220)
-        {
-            STATE = STATE_SAFE;
-            BUZZER = false;
-        }
-        else if (DISTANCE < 150)
-        {
-            STATE = STATE_WARNING;
-            TIMER = 100;
-        }
-        else
-        {}        
-      break;
-      case STATE_WARNING:
-        if (DISTANCE < 220 && TIMER > 0)
-        {
-            TIMER = TIMER - 1;
-            BUZZER = !BUZZER;
-        }
-        else if (DISTANCE >= 220)
-        {
-            STATE = STATE_SAFE;
-            BUZZER = false;
-        }
-        else if (TIMER ==0)
-        {
-            STATE = STATE_TVOFF;
-            TVON = false;
-            BUZZER = false;
-        }
-        else
-        {}
-      break;
-      case STATE_TVOFF:
-        if (DISTANCE < 220)
-        {}
-        else if (DISTANCE >= 220)
-        {
-            STATE = STATE_SAFE;
-            TVON = true;
-        }
-        else
-        {}
-      break;
-      default:
-      break;
-  }
+    // Run Finite State Machine (FSM) logic
+    switch (STATE)
+    {
+        case STATE_SAFE:
+          if (DISTANCE >= 220)
+          {}
+          else if (DISTANCE >= 150 && DISTANCE < 220)
+          {
+              STATE = STATE_REMINDER;
+              BUZ_CTR = 5;
+          }
+          else if (DISTANCE < 150)
+          {
+              STATE = STATE_WARNING;
+              TIMER = 100;
+          }
+          else
+          {}
+        break;
+        case STATE_REMINDER:
+          if (DISTANCE >= 150 && DISTANCE < 220 && BUZ_CTR > 0)
+          {
+              BUZ_CTR = BUZ_CTR - 1;
+              BUZZER = false;
+          }
+          else if (DISTANCE >= 150 && DISTANCE < 220 && BUZ_CTR == 0)
+          {
+              BUZ_CTR = 5;
+              BUZZER = true;
+          }
+          else if (DISTANCE >= 220)
+          {
+              STATE = STATE_SAFE;
+              BUZZER = false;
+          }
+          else if (DISTANCE < 150)
+          {
+              STATE = STATE_WARNING;
+              TIMER = 100;
+          }
+          else
+          {}        
+        break;
+        case STATE_WARNING:
+          if (DISTANCE < 220 && TIMER > 0)
+          {
+              TIMER = TIMER - 1;
+              BUZZER = !BUZZER;
+          }
+          else if (DISTANCE >= 220)
+          {
+              STATE = STATE_SAFE;
+              BUZZER = false;
+          }
+          else if (TIMER ==0)
+          {
+              STATE = STATE_TVOFF;
+              TVON = false;
+              BUZZER = false;
+          }
+          else
+          {}
+        break;
+        case STATE_TVOFF:
+          if (DISTANCE < 220)
+          {}
+          else if (DISTANCE >= 220)
+          {
+              STATE = STATE_SAFE;
+              TVON = true;
+          }
+          else
+          {}
+        break;
+        default:
+        break;
+    }
 
-  // Update Outputs
-  // Prints result to LCD and Serial Monitor
-  lcd.clear();
-  if (STATE == STATE_SAFE)
-  {
-    lcd.setRGB(0, 255, 0);                            // Green LCD Back Light 
-    lcd.setCursor(0, 0); lcd.print("[STAT]: "); lcd.print("SAFE");
-    Serial.print("STATE_SAFE :");    
-  }
-  else if (STATE == STATE_REMINDER)
-  {
-    lcd.setRGB(255, 255, 0);                          // Yellow LCD Back Light 
-    lcd.setCursor(0, 0); lcd.print("[STAT]: "); lcd.print("REMINDER");
-    Serial.print("STATE_REMINDER :");     
-  }
-  else if (STATE == STATE_WARNING)
-  {
-    lcd.setRGB(255, 0, 0);                            // Red LCD Back Light 
-    lcd.setCursor(0, 0); lcd.print("[STAT]: "); lcd.print("WARNING!");
-    Serial.print("STATE_WARNING :");     
-  }
-  else
-  {
-    lcd.setRGB(255, 0, 0);                            // Red LCD Back Light 
-    lcd.setCursor(0, 0); lcd.print("[STAT]: "); lcd.print("TVOFF!");
-    Serial.print("STATE_TVOFF :");       
-  }
-  lcd.setCursor(0, 1); lcd.print("[DIST]: "); lcd.print(DISTANCE);
+    // Update Outputs
+    // Prints result to LCD and Serial Monitor
+    lcd.clear();
+    if (STATE == STATE_SAFE)
+    {
+      lcd.setRGB(0, 255, 0);                            // Green LCD Back Light 
+      lcd.setCursor(0, 0); lcd.print("[STAT]: "); lcd.print("SAFE");
+      Serial.print("STATE_SAFE: ");    
+    }
+    else if (STATE == STATE_REMINDER)
+    {
+      lcd.setRGB(255, 255, 0);                          // Yellow LCD Back Light 
+      lcd.setCursor(0, 0); lcd.print("[STAT]: "); lcd.print("REMINDER");
+      Serial.print("STATE_REMINDER: ");     
+    }
+    else if (STATE == STATE_WARNING)
+    {
+      lcd.setRGB(255, 0, 0);                            // Red LCD Back Light 
+      lcd.setCursor(0, 0); lcd.print("[STAT]: "); lcd.print("WARNING!");
+      Serial.print("STATE_WARNING: ");     
+    }
+    else
+    {
+      lcd.setRGB(255, 0, 0);                            // Red LCD Back Light 
+      lcd.setCursor(0, 0); lcd.print("[STAT]: "); lcd.print("TVOFF!");
+      Serial.print("STATE_TVOFF: ");       
+    }
+    lcd.setCursor(0, 1); lcd.print("[DIST]: "); lcd.print(DISTANCE);
 
-  // Prints result to Serial Monitor
-  Serial.print("Distance to User: ");
-  Serial.print(DISTANCE);
-  Serial.println(" cm");
+    // Prints result to Serial Monitor
+    Serial.print("Distance to User: ");
+    Serial.print(DISTANCE);
+    Serial.println(" cm");
 
-  // Update BUZZER_MODULE (output module) at every cycle
-  digitalWrite(BUZZER_MODULE, BUZZER);
-  // Do something based on TVON output variable
-  if (TVON == false)
-  {
-    // Do Something (User Defined)!
-  }
+    // Update BUZZER_MODULE (output module) at every cycle
+    digitalWrite(BUZZER_MODULE, BUZZER);
+    // Do something based on TVON output variable (Optional)
+    if (TVON == false)
+    {
+      // Do Something (User Defined)!
+    }
 
-  // 100ms FSM time reference (Runs at every 100ms)
-  delay(100);
+    // 100ms FSM time reference (Runs at every 100ms)
+    delay(100);
 }
